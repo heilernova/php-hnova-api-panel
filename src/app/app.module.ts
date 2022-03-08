07676104adfae38ab3d692cbc81b5f9c2bf7b9bf
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-08 17:34:20 -0500

    Actualización de los componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { DatabasesModule } from './databases/databases.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {MatTabsModule} from '@angular/material/tabs';
+import { ApiInterceptor } from './http/api.interceptor';
+import { ApisModule } from './apis/apis.module';
+import { ConfigModule } from './config/config.module';
+import { ErrorsModule } from './errors/errors.module';
+
+import { LOCALE_ID } from '@angular/core';
+import es from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(es);
 
 @NgModule({
   declarations: [
@@ -18,9 +27,14 @@ import {MatTabsModule} from '@angular/material/tabs';
     MatIconModule,
     HttpClientModule,
     DatabasesModule,
+    ApisModule,
+    ConfigModule,
+    ErrorsModule,
     MatTabsModule
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:ApiInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
